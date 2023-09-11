4f98182a80be7a7e2e7a431184f97a8d5c3dec22
aidanmazzola <144286053+aidanmazzola@users.noreply.github.com>
2023-09-11 11:19:36 -0400

    my information v1

---
+++ myInfo.py
@@ -0,0 +1,11 @@
+Python 3.11.5 (v3.11.5:cce6ba91b3, Aug 24 2023, 10:50:31) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #CS175-50
+>>> #Aidan Mazzola
+>>> #myInfo
+>>> print("Aidan Mazzola")
+Aidan Mazzola
+>>> print("8 Oxdford Road, Manalapan, New Jersey, 07726")
+8 Oxdford Road, Manalapan, New Jersey, 07726
+>>> print("732-567-9520")
+732-567-9520
